883129056b01f73a5a6009a163ae4bb98aff2e01
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2019-03-21 11:12:23 -0400

    nav

--- Freeze/Assets/Scripts/AIMove.cs
+++ Freeze/Assets/Scripts/AIMove.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.AI;
 
-public class AIMove : MonoBehaviour
+public class AIMove : MonoBehaviourPun
 {
+    public NavMeshSurface[] maze;
+
     // Start is called before the first frame update
     void Start()
     {

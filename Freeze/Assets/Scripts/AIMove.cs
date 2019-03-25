5d2eeaa8c0eec0a269a8b9f9ac1aeb158d031eb4
BrookeAug1 <44063647+BrookeAug1@users.noreply.github.com>
2019-03-25 12:35:48 -0400

    lokjbsuife

--- Freeze/Assets/Scripts/AIMove.cs
+++ Freeze/Assets/Scripts/AIMove.cs
@@ -6,17 +6,27 @@ using UnityEngine.AI;
 
 public class AIMove : MonoBehaviourPun
 {
-    public NavMeshSurface[] maze;
-
+    private NavMeshAgent agent;
+    private GameObject currentPlayer;
+    public static List<GameObject> players;
+    private List<Paths> distances;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = this.GetComponent<NavMeshAgent>();
+        currentPlayer = players[0];
+        agent.SetDestination(currentPlayer.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Paths temp = new Paths(agent.remainingDistance, currentPlayer);
+        foreach (GameObject player in players)
+        {
+            agent.SetDestination(player.transform.position);
+            if (agent.remainingDistance < temp.GetDistance()) {
+                currentPlayer = player;  temp = new Paths(agent.remainingDistance, player);  }
+        }
     }
 }
